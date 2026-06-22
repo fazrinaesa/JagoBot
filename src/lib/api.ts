3d8e8608672c01a3265bc510bcece102dbe87efb
fazrinaesa <fazrinaesa2005@gmail.com>
2026-06-22 10:04:48 +0700

    fix: hardcode production URL with import.meta.env.PROD

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,10 @@
 import axios, { InternalAxiosRequestConfig, AxiosHeaders } from 'axios';
 
 // @ts-ignore
-const envApiUrl = import.meta.env.VITE_API_URL;
-const BASE_URL = envApiUrl
-    ? `${envApiUrl}/api`
+const BASE_URL = import.meta.env.PROD
+    ? 'https://jago-bot-upla.vercel.app/api'
     : 'http://localhost:5000/api';
+
 const api = axios.create({
     baseURL: BASE_URL,
 });
